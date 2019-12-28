4e59719d72bdcf50efeb06421d141b44ff33db62
symphony09 <54656969+symphony09@users.noreply.github.com>
2019-12-28 20:19:54 +0800

    add readme.md and some notes

---
+++ README.md
@@ -0,0 +1,13 @@
+## 程序功能
+
+爬取小米有品主页下的商品信息
+
+## 运行方法
+
+命令行（mispider目录下）运行 
+
+```powershell
+scrapy crawl goods_spider
+```
+
+
--- mispider/spiders/mi_spider.py
+++ mispider/spiders/mi_spider.py
@@ -8,6 +8,7 @@ class mispider(scrapy.Spider):
 
     def start_requests(self): 
         
+        #待爬取的URL
         urls = [
             'https://www.xiaomiyoupin.com/homepage/main/v1002?platform=pc',
         ]
@@ -17,19 +18,19 @@ class mispider(scrapy.Spider):
     def parse(self, response):
 
         goods = json.loads(response.body)
-        data = []
+        data = []  #存放商品信息
         
         for floor in goods.get("data").get("homepage").get("floors"):
             items = floor.get("data").get("items")
             if items is not None :
                 for item in items:
-                    if item.get("gid") is not None:
+                    if item.get("gid") is not None:        #除去非商品item
                         body = []
-                        body.append(item.get("gid"))
-                        body.append(item.get("name"))
-                        body.append(item.get("price_min"))
+                        body.append(item.get("gid"))       #商品id
+                        body.append(item.get("name"))      #商品名
+                        body.append(item.get("price_min")) #商品价格，最后两位为小数
                         data.append(tuple(body))
-                        
+        #将信息存入Excel                
         header = tuple(["gid","name","price"])
         tbdata = tablib.Dataset(*data,headers=header)
         open('data.xls', 'wb').write(tbdata.xls)
